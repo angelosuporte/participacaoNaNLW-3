785e14e35a93b331c6404a36256edb6a1365de89
Angelo Carvalho <angelosuporteti@gmail.com>
2020-10-15 10:08:25 -0300

    Adição do mapa 1:25

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,9 @@
+const map = L.map('mapid').setView([51.505, -0.09], 13); //códico do mapa da lib https://leafletjs.com/
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
